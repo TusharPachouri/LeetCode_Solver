4ebb3567f5eb709e09b9480680c112a1829017d5
Tushar Pachouri <tusharpachouri001@gmail.com>
2024-04-01 21:33:19 +0530

    Add Profile component and Navbar component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,38 @@
 /* eslint-disable no-unused-vars */
-import { Login } from "./components/index";
+import {
+  Login,
+  LandingPage,
+  Navbar,
+  Footer,
+  Profile,
+  AboutUs,
+  SolutionComponent,
+  DisplaySolution,
+} from "./components/index";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
     <div className="flex flex-col w-full min-h-screen">
       <BrowserRouter>
-        {/* <Nav /> */}
+        <Navbar />
         <div className="flex-grow w-full">
           {" "}
           {/* Adjusted padding for mobile */}
           <Routes>
-            {/* <Route path="/" element={<Home />} />
+            <Route path="/home" element={<LandingPage />} />
             <Route path="/about" element={<AboutUs />} />
-            <Route path="/post/:postId" element={<Post />} />
+            {/* <Route path="/post/:postId" element={<Post />} />
             <Route path="/search/:search" element={<Search />} />
-            <Route path="/contact" element={<Contact />} />
-            {/* <Route path="/profile" element={<Profile />} /> */}
+            <Route path="/contact" element={<Contact />} /> */}
+            <Route path="/profile" element={<Profile />} />
             <Route path="/login" element={<Login />} />
-            {/* <Route path="/signup" element={<SignUp />} /> */} 
+            <Route path="/solution" element={<SolutionComponent />} />
+            <Route path="/display" element={<DisplaySolution />} />
+            {/* <Route path="/signup" element={<SignUp />} />  */}
           </Routes>
         </div>
-        {/* <Footer /> */}
+        <Footer />
       </BrowserRouter>
     </div>
   );
